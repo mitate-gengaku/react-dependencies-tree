fc993dd609521129cf1b1ecf243c2bf0e56194a4
mitate-gengaku <mitategangaku@gmail.com>
2024-12-06 15:51:39 +0900

    chore: install react-dropzone

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import "@xyflow/react/dist/style.css";
+
 import {
   ReactFlow,
   useNodesState,
@@ -11,13 +13,11 @@ import {
   Connection,
   Panel,
 } from "@xyflow/react";
-import React, { useCallback } from "react";
-
-import "@xyflow/react/dist/style.css";
-import { Button } from "@/components/ui/button";
-
 import { FolderUpIcon } from "lucide-react";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -25,6 +25,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { cn } from "@/utils/cn";
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
@@ -32,8 +33,17 @@ const initialNodes = [
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
+const dirs = {
+  directory: "",
+  workingDirectory: "",
+};
+
 export default function App() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const onDrop = useCallback((files: File[]) => {
+    console.log(files);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const [nodes, _, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   const onConnect: OnConnect = useCallback(
@@ -58,9 +68,26 @@ export default function App() {
                 <FolderUpIcon />
               </Button>
             </DialogTrigger>
-            <DialogContent>
-              <DialogTitle>a</DialogTitle>
-              <DialogDescription>a</DialogDescription>
+            <DialogContent className="font-noto-sans">
+              <DialogTitle>フォルダーをアップロード</DialogTitle>
+              <DialogDescription>
+                視覚化したいフォルダーを選択・アップロードしてください。
+              </DialogDescription>
+              <div
+                {...getRootProps()}
+                className={cn(
+                  "flex h-56 w-full cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-gray-300 bg-card p-4 text-center transition-all hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600",
+                  isDragActive &&
+                    "border-gray-600 bg-gray-100 dark:border-gray-500 dark:bg-gray-600",
+                )}
+              >
+                <input {...getInputProps()} {...dirs} />
+                {isDragActive ? (
+                  <p>ドロップしてください</p>
+                ) : (
+                  <p className="text-sm">ドラッグ・アンド・ドロップ・選択</p>
+                )}
+              </div>
             </DialogContent>
           </Dialog>
         </Panel>
